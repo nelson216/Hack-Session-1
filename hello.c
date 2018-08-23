c16446b7496ea036daae04f885bc56c2710eee43
nelson216 <42561047+nelson216@users.noreply.github.com>
2018-08-23 17:49:27 -0500

    hack the best

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Ryan E & Andrew N
+ * Date: 2018/08/21
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello Andrew Nelson (Mechanical Engineer)!\n");
+
+  return 0;
+}
